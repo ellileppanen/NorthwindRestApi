f6c23e72994022adc97c42cb99698c25ebf84b87
Elli Leppänen <elli.jarvi@hotmail.com>
2025-02-03 13:08:49 +0200

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        //alustetaan tietokantayhteys
+        NorthwindOriginalContext db = new NorthwindOriginalContext();
+
+        //hakee kaikki asiakkaat
+        [HttpGet]
+        public ActionResult GetAllCustomers()
+        { 
+            var asiakkaat = db.Customers.ToList();
+            return Ok(asiakkaat);
+        }
+    }
+}
