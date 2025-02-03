e38be11946ffbdec2c00a7587bec9bc3c67fc1f1
Elli Leppänen <elli.jarvi@hotmail.com>
2025-02-03 13:38:23 +0200

    Lisäystoiminto

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -14,9 +14,57 @@ namespace NorthwindRestApi.Controllers
         //hakee kaikki asiakkaat
         [HttpGet]
         public ActionResult GetAllCustomers()
-        { 
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+        {
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest("Tapahtui virhe. Lue lisää: " +ex.InnerException);
+            }
         }
+
+        //hakee yhden asiakkaan pääavaimella
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    //return NotFound("Asiakasta id:llä " + id + " ei löydy.");
+                    return NotFound($"Asiakasta id:llä {id} ei löydy."); //string interpolation
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException);
+            }
+        }
+
+        //uuden lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody]Customer cust)
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok("Lisättiin uusi asiakas: "+ cust.CompanyName + " from:" + cust.Country);
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException);
+            }
+
+        }
+
     }
 }
