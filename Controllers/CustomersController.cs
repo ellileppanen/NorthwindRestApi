a5b967ccee017f8c8bf6f8bcb34d9e991cf4f47e
Ellinoora Leppänen <Ellinoora.Leppanen@student.careeria.fi>
2025-02-11 11:41:37 +0200

    Lisätty asiakkaan muokkaaminen ja haku nimen osalla, vaihdettu uuteen tietokantaan

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -88,7 +88,45 @@ namespace NorthwindRestApi.Controllers
             {
                 return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException);
             }
+        }
+        //asiakkaan muokkaaminen
+        [HttpPut("{id}")]
+        public ActionResult EditCustomer(string id, [FromBody] Customer customer)
+        {
+            var asiakas = db.Customers.Find(id);
+            if (asiakas != null)
+            {
+                asiakas.CompanyName = customer.CompanyName;
+                asiakas.ContactName = customer.ContactName;
+                asiakas.Address = customer.Address;
+                asiakas.City = customer.City;
+                asiakas.Region = customer.Region;
+                asiakas.PostalCode = customer.PostalCode;
+                asiakas.Country = customer.Country;
+                asiakas.Phone = customer.Phone;
+                asiakas.Fax = customer.Fax;
 
+                db.SaveChanges();
+                return Ok("Muokattu asiakasta " + asiakas.CompanyName);
+            }
+            else 
+            { 
+                return NotFound("Asiakasta ei löytynyt id:llä: " + id);
+            }
+        }
+        //hakee nimen osalla
+        [HttpGet("companyname/{cname}")]
+        public ActionResult GetByName(string cname)
+        {
+            try
+            {
+                var cust = db.Customers.Where(c => c.CompanyName.Contains(cname));
+                return Ok(cust);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
