7c6ccaa501389c0ec85b960d471b74885df36ec5
Elli Leppänen <elli.jarvi@hotmail.com>
2025-04-08 19:36:54 +0300

    users muokkaus ja poisto metodit lisätty

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -8,7 +8,12 @@ namespace NorthwindRestApi.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private static readonly NorthwindOriginalContext db = new NorthwindOriginalContext();
+        private NorthwindOriginalContext db;
+
+        public UsersController(NorthwindOriginalContext dbparametri)
+        {
+            db = dbparametri;
+        }
 
         [HttpGet]
         public ActionResult GetAll() 
@@ -16,7 +21,7 @@ namespace NorthwindRestApi.Controllers
             var users = db.Users;
 
             foreach(var user in users) 
-            {
+            { 
                 user.Password = null;
             }
             return Ok(users);
@@ -36,6 +41,55 @@ namespace NorthwindRestApi.Controllers
             catch (Exception e)
             {
                 return BadRequest("Lisääminen ei onnistunut. Tässä lisää tietoa: " + e);
+            }
+        }
+
+        //muokkaaminen
+        [HttpPut("{id}")]
+        public ActionResult EditUser(int id, [FromBody] User user)
+        {
+            var käyttäjä = db.Users.Find(id);
+            if (käyttäjä != null)
+            {
+                käyttäjä.FirstName = user.FirstName;
+                käyttäjä.LastName = user.LastName;
+                käyttäjä.Email = user.Email;
+                käyttäjä.UserName = user.UserName;
+                käyttäjä.Password = user.Password;
+                käyttäjä.AccesslevelId = user.AccesslevelId;
+
+
+                db.SaveChanges();
+                return Ok("Muokattu käyttäjää " + käyttäjä.UserName);
+            }
+            else
+            {
+                return NotFound("Käyttäjää ei löytynyt id:llä: " + id);
+            }
+        }
+
+        //käyttäjän poistaminen
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var käyttäjä = db.Users.Find(id);
+
+                if (käyttäjä != null)
+                {
+                    db.Users.Remove(käyttäjä);
+                    db.SaveChanges();
+                    return Ok("Käyttäjä " + käyttäjä.LastName + " " + käyttäjä.FirstName + " poistettiin onnistuneesti.");
+                }
+                else
+                {
+                    return NotFound("Käyttäjää id:llä " + id + " ei löytynyt.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException);
             }
         }
 
